dcdff9f58a37ed860cc6721af4975c3826330b65
nerdeveloper <odirionye@gmail.com>
2019-09-18 13:53:28 +0100

    add webpack config

---
+++ src/js/main.js
@@ -0,0 +1,18 @@
+import "../css/custom.css";
+import "../js/vendors/bootstrap.bundle.min.js";
+import "./vendors/hammer.js";
+import {$, jquery} from  "../js/vendors/jquery.min.js";
+import  "../js/vendors/jquery.easing.min.js";
+import   "./vendors/jquery.magnific-popup.min.js";
+
+import {pace} from "./vendors/pace.js";
+import "./vendors/swiper.min.js"
+import 'owl.carousel/dist/assets/owl.carousel.min.css';
+import 'owl.carousel';
+import 'ionicons/dist/css/ionicons.min.css'
+import "ionicons"
+import "../js/custom.js"
+// export for others scripts to use
+window.$ = $;
+window.jquery = jquery;
+window.pace = pace;
