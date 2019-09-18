aaa659b8050c6f2071dda12a691a48a87a0c466b
nerdeveloper <odirionye@gmail.com>
2019-09-18 20:50:30 +0100

    remove unused js and css files

--- src/js/main.js
+++ src/js/main.js
@@ -4,7 +4,6 @@ import "../js/vendors/bootstrap.bundle.min.js";
 import "./vendors/hammer.js";
 import "../js/vendors/jquery.easing.min.js";
 import "./vendors/jquery.magnific-popup.min.js";
-
 import { pace } from "./vendors/pace.js";
 import "./vendors/swiper.min.js"
 import 'owl.carousel/dist/assets/owl.carousel.min.css';
