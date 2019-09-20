1aa0253b520da410413f30ffafaf54f029b69427
simplytammy <ogurinkabenjamin@gmail.com>
2019-09-20 13:11:21 +0100

    fix conflicts and merge commits

--- src/js/main.js
+++ src/js/main.js
@@ -3,18 +3,9 @@ import "../css/custom.css";
 import "./vendors/hammer.js";
 import "../js/vendors/jquery.easing.min.js";
 import "./vendors/jquery.magnific-popup.min.js";
-import {
-	pace
-}
-from "./vendors/pace.js";
 import "./vendors/swiper.min.js"
 import 'owl.carousel/dist/assets/owl.carousel.min.css';
 import 'owl.carousel';
 import 'ionicons/dist/css/ionicons.min.css';
 import "ionicons";
 import "../js/custom.js";
-// export for others scripts to use
-window.$ = $;
-window.jQuery = $;
-window.jquery = $;
-window.pace = pace;
